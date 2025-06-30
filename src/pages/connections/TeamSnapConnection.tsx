66a94c8623ded10b9c76b9658e1c414e3655dd84
markyearling <101452827+markyearling@users.noreply.github.com>
2025-06-30 00:02:05 -0500

    Updated TeamSnapConnection.tsx

--- src/pages/connections/TeamSnapConnection.tsx
+++ src/pages/connections/TeamSnapConnection.tsx
@@ -47,7 +47,7 @@ const TeamSnapConnection: React.FC = () => {
   const { profiles } = useProfiles();
 
   const teamSnap = new TeamSnapService({
-    clientId: process.env.VITE_TEAMSNAP_CLIENT_ID || '',
+    clientId: process.env.EXPO_PUBLIC_TEAMSNAP_CLIENT_ID || '',
     redirectUri: `${window.location.origin}/connections/teamsnap/callback`
   });
 
