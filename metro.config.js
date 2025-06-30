f9297eab1a95776c25d9f37e48131c5201ae0cb5
markyearling <101452827+markyearling@users.noreply.github.com>
2025-06-30 09:50:59 -0500

    Fix duplicated code in metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -15,30 +15,6 @@ config.transformer.getTransformOptions = async () => ({
   },
 });
 
-// Remove the watcher options that were causing validation warnings
-// config.watcher = {
-//   usePolling: true,
-//   interval: 1000,
-// };
-
-module.exports = config;
-// metro.config.js
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-config.watchFolders = [];
-config.server = {
-  port: 8081,
-};
-
-config.transformer.getTransformOptions = async () => ({
-  transform: {
-    experimentalImportSupport: false,
-    inlineRequires: true,
-  },
-});
-
 // 🔥 This is what avoids the EMFILE crash:
 config.watch = {
   usePolling: true,
